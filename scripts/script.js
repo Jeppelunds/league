36f2ae1d1847114a05a0b86f32b1447d96ecc930
Jeppelunds <mickijeppe@gmail.com>
2023-04-08 13:32:31 +0200

    Commit

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,40 @@ const submitButton = document.querySelector(".submitButton");
 let champSearch;
 
 submitButton.addEventListener("click", function () {
+if (searchBar.value === "Wukong") {
+  searchBar.value = "MonkeyKing";
+} else if (searchBar.value === "Lee Sin") {
+  searchBar.value = "LeeSin";
+} else if (searchBar.value === "Miss Fortune") {
+  searchBar.value = "MissFortune";
+} else if (searchBar.value === "Aurelion Sol") {
+  searchBar.value = "AurelionSol";
+} else if (searchBar.value === "Dr. Mundo") {
+  searchBar.value = "DrMundo";
+} else if (searchBar.value === "Lee Sin") {
+  searchBar.value = "LeeSin";
+} else if (searchBar.value === "Jarvan IV") {
+  searchBar.value = "JarvanIV";
+} else if (searchBar.value === "K'sante") {
+  searchBar.value = "KSante";
+} else if (searchBar.value === "Kai'sa") {
+  searchBar.value = "Kaisa";
+} else if (searchBar.value === "Kha'zix") {
+  searchBar.value = "Khazix";
+} else if (searchBar.value === "Kog'maw") {
+  searchBar.value = "KogMaw";
+} else if (searchBar.value === "Master Yi") {
+  searchBar.value = "MasterYi";
+} else if (searchBar.value === "Rek'sai") {
+  searchBar.value = "RekSai";
+} else if (searchBar.value === "Renata Glasc") {
+  searchBar.value = "Renata";
+} else if (searchBar.value === "Tahm Kench") {
+  searchBar.value = "TahmKench";
+} else if (searchBar.value === "Twisted Fate") {
+  searchBar.value = "TwistedFate";
+}
+
   champSearch = searchBar.value;
 
   console.log(champSearch);
@@ -20,7 +54,7 @@ submitButton.addEventListener("click", function () {
       console.log(data);
       const gamer = data.data;
       const array = gamer[champSearch].skins;
-      champName.innerHTML = `<h1>${gamer[champSearch].id}</h1><h2>${gamer[champSearch].title}</h2>`;
+      champName.innerHTML = `<h1>${gamer[champSearch].name}</h1><h2>${gamer[champSearch].title}</h2>`;
       let skinsHTML = "";
       array.forEach((skin) => {
         skinsHTML += `<div>
